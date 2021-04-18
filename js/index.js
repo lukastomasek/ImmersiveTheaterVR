aebc95bdd3fdf1533f9c7ec64a18547aed034165
Lukas Tomasek <vedstav31@gmail.com>
2021-04-18 18:43:41 -0400

    added nav and footer

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
+
+
+var scene = new THREE.Scene();
+
+
+
+
+
+window.addEventListener('mousemove', onMouseMove);
+render();
