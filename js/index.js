1a13f8aee36beb9d9edec8282815922dea14b377
Lukas Tomasek <vedstav31@gmail.com>
2021-04-19 19:16:32 -0400

    added styling

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
-
-
 const animateLogo = () => gsap.to(".logo", {duration: 1.5, x: 50, ease: 'sine'})
 
-
+const animateMainLogo = () => {
+  gsap.from("#main-logo",{
+    opacity: 0, 
+    y: 100, 
+    duration: 4,
+    ease:'elastic'
+  })
+}
+const animateMainP = () => {
+  gsap.from("#main-p",{
+    opacity: 0,  
+    duration: 6,
+    ease:'sine'
+  })
+}
 
 
 animateLogo()
+
+animateMainLogo()
+
+animateMainP()
