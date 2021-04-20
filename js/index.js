1336ffe0db702bf812ea3ccb460be5c38698b9b7
Lukas Tomasek <vedstav31@gmail.com>
2021-04-20 17:51:18 -0400

    finished work and render section

--- js/index.js
+++ js/index.js
@@ -1,24 +1,46 @@
-const animateLogo = () => gsap.to(".logo", {duration: 1.5, x: 50, ease: 'sine'})
 
-const animateMainLogo = () => {
-  gsap.from("#main-logo",{
-    opacity: 0, 
-    y: 100, 
-    duration: 4,
-    ease:'elastic'
-  })
-}
-const animateMainP = () => {
-  gsap.from("#main-p",{
-    opacity: 0,  
-    duration: 6,
-    ease:'sine'
-  })
+const $about = document.getElementById('about-btn')
+const $work = document.getElementById('work-btn')
+const $renders = document.getElementById('renders-btn')
+
+let slideIndex = 1
+showSlides(slideIndex)
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n)
 }
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
 
-// animateLogo()
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"
+  dots[slideIndex-1].className += " active"
+}
 
-// animateMainLogo()
-
-// animateMainP()
+$about.addEventListener('click',()=>{
+  let ele = document.querySelector(".about");
+  ele.scrollIntoView({ block: "end", behavior: "smooth" });    
+})
+$work.addEventListener('click',()=>{
+  let ele = document.querySelector(".work");
+  ele.scrollIntoView({ block: "start", behavior: "smooth" });   
+})
+$renders.addEventListener('click',()=>{
+  let ele = document.querySelector(".renders");
+  ele.scrollIntoView({ block: "start", behavior: "smooth" });   
+})
